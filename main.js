265843b852fda51273860972ee9bb33bd7825f22
DewDropDolphin <83781962+DewDropDolphin@users.noreply.github.com>
2021-07-02 10:29:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+function q1() {
+    window.alert("Anxiety can make you feel like it's never ending, and that's no fun. Sometimes you just need to be reminded of that fact, so consider this as an option to your anxious friend. Maybe that little bit of advice will remind them that there is hope in the future. (This feeling won't last forever.) ");
+}
+function q2() {
+    window.alert("Anxious people can feel alone, particularly when they're stuck in a feedback loop. By reminding them that other people feel this way — including you — that means there's at least one other person who knows exactly what they're feeling. Relate a personal story, but make sure to keep the focus on the anxious person and not turn the light on yourself. Just explain how you know what it's like, then continue to help them through it.(I feel the same way sometimes, and it's not fun.)");
+}
+function q3() {
+    window.alert("This hits on the same point as above, just in a different direction. There have been thousands of years of humans, and chances are pretty good this problem has come up before. That kind of tip can help.(You're not the only one that's ever felt this way.)");
+}
+function q4() {
+    window.alert("Again, this is about acknowledgement, which is important. It is OK to feel anxious, and that's not always fresh in your mind when going through this kind of thing, so remind your friend of that. (Sometimes it's OK to be anxious.)");
+}
+function q5() {
+    window.alert("This serves two purposes. First, it lets them know that they're not alone in the journey, by using 'we'. Second, it walks them through the process of what's worrying them, so you two can troubleshoot and solve it. What's the first thing we need to worry about? ");
+}
+function q6() {
+    window.alert("Anxious people can feel alone, particularly when they're stuck in a feedback loop. By reminding them that other people feel this way — including you — that means there's at least one other person who knows exactly what they're feeling. Relate a personal story, but make sure to keep the focus on the anxious person and not turn the light on yourself. Just explain how you know what it's like, then continue to help them through it.I feel the same way sometimes, and it's not fun.");
+}
+function q7() {
+    window.alert("Anxious people can feel alone, particularly when they're stuck in a feedback loop. By reminding them that other people feel this way — including you — that means there's at least one other person who knows exactly what they're feeling. Relate a personal story, but make sure to keep the focus on the anxious person and not turn the light on yourself. Just explain how you know what it's like, then continue to help them through it. I feel the same way sometimes, and it's not fun.");
+}
+function q8() {
+    window.alert("This hits on the same point as above, just in a different direction. There have been thousands of years of humans, and chances are pretty good this problem has come up before. That kind of tip can help.");
+}
+function q9() {
+    window.alert("Again, this is about acknowledgement, which is important. It is OK to feel anxious, and that's not always fresh in your mind when going through this kind of thing, so remind your friend of that.  Sometimes it's OK to be anxious.");
+}
+function q10() {
+    window.alert("This serves two purposes. First, it lets them know that they're not alone in the journey, by using  we.  Second, it walks them through the process of what's worrying them, so you two can troubleshoot and solve it.  What's the first thing we need to worry about?");
+}
+function q11() {
+    window.alert("I'm proud of you already. When you're feeling low it's hard to imagine that anyone could feel anything for you, and this reminds your anxious friend how you feel. And if you are proud of them, this could pull them up that much more.");
+}
+function q12() {
+    window.alert("I know this is difficult. Acknowledgement is so key to all of this, and a simple statement like this can help smooth things out a lot.");
+}
+function q13() {
+    window.alert("We can work through this together.  One person in a storm can struggle to deal with the wind, but when they've got someone to help prop them up, the two are stronger as a whole.");
+}
+function q14() {
+    window.alert("Remember when you did XX before?  We all have gone through tough times in the past, and if you remind your friend that they've done something similar before, then they'll realize that there is an end game.");
+}
+function q15() {
+    window.alert(" This is a judgment-free area, so let's talk about it.  Anxiety can make you think irrationally, and that even your closest friend is against you. By laying it out, you're making it clear that there will be no stones thrown in this situation.");
+}
+function q16() {
+    window.alert("You can call me anytime you want.  Sometimes the situation can't be resolved right away, and even if it can, the person may relapse. Let them know that you're there, whenever they need them, and are just a phone call away.");
+}
+function q17() {
+    window.alert("Not everything is going to be fair, and that's OK.  The cold realities of the world can be shocking, but with acceptance things can get better.");
+}
+function q18() {
+    window.alert("Once you get through this, you'll be glad that you made it.  Pushing through the problem can be hard, but you'll be better for it in the end. Remind them of that.");
+}
+function q19() {
+    window.alert("You don't need to fight your feelings. It's easy for the anxious person to feel like they need to deny their own feelings, so let them know that it's OK for them to have those emotions.");
+}
+function q20() {
+    window.alert("You're exactly where you are today because that's where you need to be. This is a bit more philosophical, but the premise is pretty simple. If this is destiny, then you'll be all the better for it in the end.");
+}
+function q21() {
+    window.alert("Nobody is going to remember this moment in five years or five months. Time is temporary, as is pain. Nothing is permanent and even people in the dumps can come back. There's no better example of that than John Travolta and Ben Affleck, who both made horrific career mistakes and then came back to glory.");
+}
+function q22() {
+    window.alert("There's a lesson hidden in everything. If you can learn from your mistakes, then you'll be all the better in the long run. Figure out what they can take from what's going on and make it into a positive thing.");
+}
+function q23() {
+    window.alert("Tell me the worst thing that could happen. The important part here is not to ask this sentence as a question, but instead to ask them to explain how they see the situation going down. Talking helps.");
+}
+function q24() {
+    window.alert(" Let's look for some evidence. Their anxiety may be based in fear, which could be irrational. Looking for evidence of their fears shows them the flaws in that logic.");
+}
+function q25() {
+    window.alert(" This is about you. You might be tempted to start this sentence with,  It's not about me, this is about you,  but that can come off as combative. Instead, just focus on how you're there for them, and how you should stay out of the equation.");
+}
+function q26() {
+    window.alert(" You are safe. Anxious people may feel like the world is crashing down around them, and there's nowhere to hide. You're telling them they're in a safe place, which always helps.");
+}
+function q27() {
+    window.alert(" Tell me more about your experience. Get them talking, because then you're both involved and helping each other.");
+}
+function q28() {
+    window.alert("I'm sorry you're going through that. This isn't about condescension; make sure that you're sincere in your wording.");
+}
+function q29() {
+    window.alert("That must be hard for you. Sincerity, just like in the previous one, is the important part of this phrase. You're empathizing, not judging.");
+}
+function q30() {
+    window.alert("Just listen. Sometimes, all your friend wants to do is talk things out, not hear potential solutions, but they can't necessarily articulate that need. Just listen to them and their complaints, nodding and understanding as you go, only asking questions for clarification. That may be their best option.");
+}
+function q31() {
+    window.alert("You're going to be alright. Let them know that this feeling or situation isn’t forever. Even if they don’t feel like it will, things will improve.");
+}
